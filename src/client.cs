feaf9cd1a16c0b6fd94b4cbbc9dcb3cd1551ffbd
Cédric Belin <cedric@belin.io>
2025-02-16 13:37:35 +0100

    Update the solution

---
+++ src/client.cs
@@ -0,0 +1,8 @@
+﻿namespace Belin.FreeMobile;
+
+/// <summary>
+/// Sends messages by SMS to a <see href="https://mobile.free.fr">FreeMobile</see> account.
+/// </summary>
+public class Client {
+
+}
---
+++ test/client_test.cs
@@ -0,0 +1,8 @@
+﻿namespace Belin.FreeMobile;
+
+/// <summary>
+/// Tests the features of the <see cref="Client"/> class.
+/// </summary>
+[TestClass]
+public sealed class ClientTest {
+}
