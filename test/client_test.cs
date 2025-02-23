44eb277472408c44016acf9e77a84989608cbed6
Cédric Belin <cedric@belin.io>
2025-02-23 02:08:12 +0100

    Code optimization

--- test/client_test.cs
+++ test/client_test.cs
@@ -7,19 +7,19 @@ namespace Belin.FreeMobile;
 public sealed class ClientTest {
 
 	[TestMethod("It should throw a `HttpRequestException` if a network error occurred")]
-	public async Task NetworkErrorAsync() {
+	public async Task NetworkError() {
 		var client = new Client("anonymous", "secret", "http://localhost:10000");
 		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!"));
 	}
 
 	[TestMethod("It should throw a `HttpRequestException` if the credentials are invalid")]
-	public async Task InvalidCredentialsAsync() {
+	public async Task InvalidCredentials() {
 		var client = new Client("anonymous", "secret");
 		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!"));
 	}
 
 	[TestMethod("It should send SMS messages if the credentials are valid")]
-	public async Task ValidCredentialsAsync() {
+	public async Task ValidCredentials() {
 		var client = new Client(Environment.GetEnvironmentVariable("FREEMOBILE_ACCOUNT")!, Environment.GetEnvironmentVariable("FREEMOBILE_API_KEY")!);
 		await client.SendMessage("Hello Cédric, from .NET!");
 	}
