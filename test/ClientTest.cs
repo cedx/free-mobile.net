9f01bfc36ac555ee92ddee3885c969a1ebc6c167
Cédric Belin <cedx@outlook.com>
2025-10-08 12:18:18 +0200

    Update the dependencies

--- test/ClientTest.cs
+++ test/ClientTest.cs
@@ -6,20 +6,23 @@ namespace Belin.FreeMobile;
 [TestClass]
 public sealed class ClientTest {
 
-	[TestMethod("It should throw a `HttpRequestException` if a network error occurred")]
+	[TestMethod]
 	public async Task NetworkError() {
+		// It should throw a `HttpRequestException` if a network error occurred.
 		var client = new Client("anonymous", "secret", "http://localhost:10000");
 		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!"));
 	}
 
-	[TestMethod("It should throw a `HttpRequestException` if the credentials are invalid")]
+	[TestMethod]
 	public async Task InvalidCredentials() {
+		// It should throw a `HttpRequestException` if the credentials are invalid.
 		var client = new Client("anonymous", "secret");
 		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!"));
 	}
 
-	[TestMethod("It should send SMS messages if the credentials are valid")]
+	[TestMethod]
 	public async Task ValidCredentials() {
+		// It should send SMS messages if the credentials are valid.
 		var client = new Client(Environment.GetEnvironmentVariable("FREEMOBILE_ACCOUNT")!, Environment.GetEnvironmentVariable("FREEMOBILE_API_KEY")!);
 		await client.SendMessage("Hello Cédric, from .NET!");
 	}
