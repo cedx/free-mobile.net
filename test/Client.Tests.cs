82a3abc2885ada84930bb56d63598053be3617b8
Cédric Belin <cedx@outlook.com>
2025-10-11 13:50:00 +0200

    Rename the test files [skip ci]

--- test/Client.Tests.cs
+++ test/Client.Tests.cs
@@ -4,7 +4,7 @@ namespace Belin.FreeMobile;
 /// Tests the features of the <see cref="Client"/> class.
 /// </summary>
 [TestClass]
-public sealed class ClientTest {
+public sealed class ClientTests {
 
 	[TestMethod]
 	public async Task NetworkError() {
