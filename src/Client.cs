c1691223a3014d4f60344f16804378c47791d836
Cédric Belin <cedx@outlook.com>
2025-11-02 13:51:26 +0100

    Add a constructor that accepts a `System.Net.NetworkCredential` object for the Free Mobile username and password

--- src/Client.cs
+++ src/Client.cs
@@ -1,15 +1,13 @@
 namespace Belin.FreeMobile;
 
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using System.Net.Http.Headers;
 
 /// <summary>
 /// Sends messages by SMS to a <see href="https://mobile.free.fr">FreeMobile</see> account.
 /// </summary>
-/// <param name="account">The Free Mobile account.</param>
-/// <param name="apiKey">The Free Mobile API key.</param>
-/// <param name="baseUrl">The base URL of the remote API endpoint.</param>
-public class Client(string account, string apiKey, [StringSyntax(StringSyntaxAttribute.Uri)] string baseUrl = "https://smsapi.free-mobile.fr") {
+public class Client {
 
 	/// <summary>
 	/// The base URL of the remote API endpoint.
@@ -22,9 +20,23 @@ public class Client(string account, string apiKey, [StringSyntax(StringSyntaxAtt
 	private NetworkCredential credential;
 
 	/// <summary>
-	/// The base URL of the remote API endpoint.
+	/// Creates a new client.
 	/// </summary>
-	public Uri BaseUrl => new(baseUrl.EndsWith('/') ? baseUrl : $"{baseUrl}/");
+	/// <param name="credential">The Free Mobile username and password.</param>
+	/// <param name="baseUrl">The base URL of the remote API endpoint.</param>
+	public Client(NetworkCredential credential, [StringSyntax(StringSyntaxAttribute.Uri)] string baseUrl = "https://smsapi.free-mobile.fr") {
+		this.BaseUrl = new(baseUrl.EndsWith('/') ? baseUrl : $"{baseUrl}/");
+		this.credential = credential;
+	}
+
+	/// <summary>
+	/// Creates a new client.
+	/// </summary>
+	/// <param name="username">The Free Mobile username.</param>
+	/// <param name="password">The Free Mobile password.</param>
+	/// <param name="baseUrl">The base URL of the remote API endpoint.</param>
+	public Client(string username, string password, [StringSyntax(StringSyntaxAttribute.Uri)] string baseUrl = "https://smsapi.free-mobile.fr"):
+		this(new NetworkCredential(username, password), baseUrl) {}
 
 	/// <summary>
 	/// Sends an SMS message to the underlying account.
@@ -34,7 +46,7 @@ public class Client(string account, string apiKey, [StringSyntax(StringSyntaxAtt
 	/// <returns>Completes when the message has been sent.</returns>
 	/// <exception cref="HttpRequestException">The HTTP response is unsuccessful.</exception>
 	public async Task SendMessage(string text, CancellationToken cancellationToken = default) {
-		using var request = await CreateRequest(text);
+		using var request = await CreateRequest(text, cancellationToken);
 		using var httpClient = new HttpClient();
 		using var response = await httpClient.SendAsync(request, cancellationToken);
 		response.EnsureSuccessStatusCode();
@@ -44,16 +56,17 @@ public class Client(string account, string apiKey, [StringSyntax(StringSyntaxAtt
 	/// Creates the HTTP request corresponding to the specified message.
 	/// </summary>
 	/// <param name="text">The message text.</param>
+	/// <param name="cancellationToken">The token to cancel the operation.</param>
 	/// <returns>The newly created HTTP request message.</returns>
-	private async Task<HttpRequestMessage> CreateRequest(string text) {
+	private async Task<HttpRequestMessage> CreateRequest(string text, CancellationToken cancellationToken = default) {
 		var trimmedText = text.Trim();
 		using var query = new FormUrlEncodedContent(new Dictionary<string, string> {
 			["msg"] = trimmedText.Length > 160 ? trimmedText[0..160] : trimmedText,
-			["pass"] = ApiKey,
-			["user"] = Account
+			["pass"] = credential.Password,
+			["user"] = credential.UserName
 		});
 
-		var request = new HttpRequestMessage(HttpMethod.Get, new Uri(BaseUrl, $"sendmsg?{await query.ReadAsStringAsync()}"));
+		var request = new HttpRequestMessage(HttpMethod.Get, new Uri(BaseUrl, $"sendmsg?{await query.ReadAsStringAsync(cancellationToken)}"));
 		request.Headers.UserAgent.Add(new ProductInfoHeaderValue(".NET", Environment.Version.ToString(3)));
 		return request;
 	}
