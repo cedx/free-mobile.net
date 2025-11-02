4ff8f3b90dc95ed5a09179924841886938992898
Cédric Belin <cedx@outlook.com>
2025-11-02 13:49:37 +0100

    Update the tests

--- test/Client.Tests.cs
+++ test/Client.Tests.cs
@@ -3,27 +3,28 @@ namespace Belin.FreeMobile;
 /// <summary>
 /// Tests the features of the <see cref="Client"/> class.
 /// </summary>
+/// <param name="testContext">The test context.</param>
 [TestClass]
-public sealed class ClientTests {
+public sealed class ClientTests(TestContext testContext) {
 
 	[TestMethod]
 	public async Task NetworkError() {
 		// It should throw a `HttpRequestException` if a network error occurred.
 		var client = new Client("anonymous", "secret", "http://localhost:666");
-		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!"));
+		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!", testContext.CancellationToken));
 	}
 
 	[TestMethod]
 	public async Task InvalidCredentials() {
 		// It should throw a `HttpRequestException` if the credentials are invalid.
 		var client = new Client("anonymous", "secret");
-		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!"));
+		await ThrowsAsync<HttpRequestException>(() => client.SendMessage("Hello World!", testContext.CancellationToken));
 	}
 
 	[TestMethod]
 	public async Task ValidCredentials() {
 		// It should send SMS messages if the credentials are valid.
 		var client = new Client(Environment.GetEnvironmentVariable("FREEMOBILE_ACCOUNT")!, Environment.GetEnvironmentVariable("FREEMOBILE_API_KEY")!);
-		await client.SendMessage("Hello Cédric, from .NET!");
+		await client.SendMessage("Hello Cédric, from .NET!", testContext.CancellationToken);
 	}
 }
