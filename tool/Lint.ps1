fb3945c990a6be0e5e8b9f4b8aff9e941bd3b3e7
Cédric Belin <cedx@outlook.com>
2025-11-22 23:58:22 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,4 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
-Test-ModuleManifest "$PSScriptRoot/../FreeMobile.psd1" | Out-Null
+Test-ModuleManifest FreeMobile.psd1 | Out-Null
