699172e36ae454064160a7d178538b448ef1749b
Cédric Belin <cedx@outlook.com>
2025-12-01 23:16:45 +0100

    Add the PowerShell cmdlets

---
+++ src/FreeMobile.Cmdlets/Send-Message.cs
@@ -0,0 +1,48 @@
+namespace Belin.FreeMobile.Cmdlets;
+
+using System.Net;
+
+/// <summary>
+/// Sends an SMS message to the specified Free Mobile account.
+/// </summary>
+[Cmdlet(VerbsCommunications.Send, "Message", DefaultParameterSetName = "Credential")]
+[OutputType(typeof(void))]
+public class SendMessage: PSCmdlet {
+
+	/// <summary>
+	/// The Free Mobile client to use.
+	/// </summary>
+	[Parameter(Mandatory = true, ParameterSetName = "Client")]
+	public required Client Client { get; set; }
+
+	/// <summary>
+	/// The Free Mobile user name and password.
+	/// </summary>
+	[Parameter(Mandatory = true, ParameterSetName = "Credential"), Credential]
+	public required PSCredential Credential { get; set; }
+
+	/// <summary>
+	/// The message text.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+	public required string Message { get; set; }
+
+	/// <summary>
+	/// The base URL of the remote API endpoint.
+	/// </summary>
+	[Parameter(ParameterSetName = "Credential")]
+	public Uri? Uri { get; set; }
+
+	/// <summary>
+	/// Performs initialization of command execution.
+	/// </summary>
+	protected override void BeginProcessing() {
+		if (ParameterSetName == "Credential") Client = new Client((NetworkCredential) Credential, Uri);
+	}
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() =>
+		Client.SendMessage(Message).GetAwaiter().GetResult();
+}
