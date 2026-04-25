8716f9feb9d07f9e33e96da2146c0f6045fc1053
Cédric Belin <cedx@outlook.com>
2026-04-25 12:31:30 +0200

    Remove the PowerShell examples

--- example/Cmdlets/Send-FreeMobileMessage.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-	Sends an SMS notification.
-#>
-Import-Module Belin.FreeMobile
-
-$credential = [pscredential]::new("Your account identifier", (ConvertTo-SecureString "Your API key" -AsPlainText))
-Send-FreeMobileMessage "Hello World from PowerShell!" -Credential $credential
-Write-Output "The message was sent successfully."
